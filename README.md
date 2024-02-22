70aaa22cc4db4354b749612abda5d018cb23f0eb
Samuel-Attivi <Samuel.attivi247@gmail.com>
2024-02-22 18:17:14 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# mypackage
+This library was created on how to publish your own python package
+
+# how to install 
+
---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mypackage",
+    version="0.1",
+    packages=find_packages(exclude=["tests"]), 
+    license="MIT", 
+    description="EDA example python package", 
+    long_description=open("README.md").read(),
+    include_requires=["numpy"], 
+    url="", 
+    author="Samuel Attivi", 
+    author_email="Samuelattivi4@gmail.com"
+    
+)
